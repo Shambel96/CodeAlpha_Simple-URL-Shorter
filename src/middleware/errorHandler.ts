1c7f735134f5af6c33d114e7fbc0426ed53fd8b6
Shambel Dechu <shambeldechu@gmail.com>
2026-08-14 00:04:17 +0300

    update

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,12 +1,21 @@
 import type { ErrorRequestHandler } from "express";
+import { Prisma } from "@prisma/client";
 import { AppError } from "../errors/AppError.js";
 
 export const errorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
   console.error(error);
-
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({
       message: error.message,
+    });
+  }
+
+  if (
+    error instanceof Prisma.PrismaClientKnownRequestError &&
+    error.code === "P2002"
+  ) {
+    return res.status(409).json({
+      message: "A resource with the same value already exists",
     });
   }
 
