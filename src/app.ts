b464e0fc733d9bf5e5e1f95aa7dddc8a53a2e004
Shambel Dechu <shambeldechu@gmail.com>
2026-08-14 00:04:03 +0300

    feat: add errorhandler and notFound handler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
-
 import urlRouter from "./routes/url.routes.js";
 import redirectRouter from "./routes/redirect.routes.js";
+import { notFoundHandler } from "./middleware/notFound.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
 
@@ -14,7 +15,8 @@ app.get("/", (_req, res) => {
 });
 
 app.use("/api/urls", urlRouter);
-
 app.use("/", redirectRouter);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 export default app;
