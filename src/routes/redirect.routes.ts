018eada71e141b463315f8a760a70ca41e9f0f7f
Shambel Dechu <shambeldechu@gmail.com>
2026-08-13 23:59:20 +0300

    chore: update the routes to accept middleware

--- src/routes/redirect.routes.ts
+++ src/routes/redirect.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { redirectUrl } from "../controllers/url.controller.js";
+import { asyncHandler } from "../middleware/asyncHandler.js";
 
 const router = Router();
 
-router.get("/:code", redirectUrl);
+router.get("/:code", asyncHandler(redirectUrl));
 
 export default router;
--- src/routes/url.routes.ts
+++ src/routes/url.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { createUrl } from "../controllers/url.controller.js";
+import { validate } from "../middleware/validate.js";
+import { asyncHandler } from "../middleware/asyncHandler.js";
+import { createUrlSchema } from "../validators/url.validator.js";
 
 const router = Router();
-router.post("/", createUrl);
+
+router.post("/", validate(createUrlSchema), asyncHandler(createUrl));
 
 export default router;
