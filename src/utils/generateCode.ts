212f05cc5f0014d6341b76040ac83409298a0aff
Shambel Dechu <shambeldechu@gmail.com>
2026-08-13 12:45:13 +0300

    chore: project setup fully

---
+++ src/utils/generateCode.ts
@@ -0,0 +1,14 @@
+const characters =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+export function generateCode(length = 6): string {
+  let code = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+
+    code += characters[randomIndex];
+  }
+
+  return code;
+}
